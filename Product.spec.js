66534605e4218e7f01d301732b6381fa54312f69
Silvana Duarte <smd54789@gmail.com>
2026-09-24 18:50:23 +0200

    24/08

--- Product.spec.js
+++ Product.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 import { login } from './login'
 
+//without login
 test('Product - Information', async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/")
     await page.locator('[data-test="search-query"]').fill("slip")
@@ -14,5 +15,15 @@ test('Product - Information', async ({ page }) => {
     await page.getByLabel('price').toHaveText('9.17')
     await expect(page.locator('.co2-letter.active')).toHaveClass(/active/)
     await page.locator('[data-test="product-description"]').toHaveText("Adjustable slip joint pliers that feature a two-position pivot mechanism for switching between regular and wide jaw openings to accommodate different fastener sizes. The PVC-coated handles offer a comfortable, non-slip grip while the precisely machined jaws securely grasp round, flat, and hexagonal objects without slipping. Constructed from durable drop-forged carbon steel that has been heat-treated to withstand heavy daily use in professional environments. The curved jaw section grips pipes and cylindrical objects, while the flat front surface handles nuts, bolts, and sheet materials. A wire cutter integrated near the pivot handles light cutting tasks without reaching for a separate tool. Perfect for plumbing, automotive, HVAC, and general maintenance work where a single versatile gripping tool saves time.")
-    await page.locator('')
+    await page.locator('[data-test="quantity"]').toHaveText('1')
+    await page.locator('[data-test="increase-quantity"]').click()
+    await page.locator('[data-test="quantity"]').toHaveText('2')
+    await page.locator('[data-test="decrease-quantity"]').click()
+    await page.locator('[data-test="quantity"]').toHaveText('1')
+    await page.locator('[data-test="add-to-cart"]').click()
+    await page.getByRole('alert').toHaveText('Product added to shopping cart.')
+    await page.locator('[data-test="add-to-favorites"]').click()
+    await page.getByRole('alert').toHaveText('Unauthorized can not add product to favorite list.')
+    await page.getByRole('button', 
+
 })
